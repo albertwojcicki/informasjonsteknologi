fdb67cd04ea341fbc7122e36ce1d41ab036e4b69
yoyo1 <albert.wojcicki06@gmail.com>
2024-05-22 11:16:59 +0200

    jobber på registrer og logg inn på forntend

--- client/app.py
+++ client/app.py
@@ -10,6 +10,28 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/registrer")
+def registrer():
+    return render_template("registrer.html")
+
+@app.route("/logginn")
+def logginn():
+    return render_template("logginn.html")
+
+@app.route("/registrer_bruker", metohds=["POST"])
+def registrer_bruker():
+    name = request.form.get("name")
+    password = request.form.get("password")
+    data = {
+        "name": name, 
+        "password": password
+    }
+    requests("http://127.0.0.1:5010")
+
+@app.route("/logginn_bruker", metohds=["POST"])
+def logginn_bruker():
+    name = request.form.get("name")
+    password = request.form.get("password")
 
 if __name__ == "__main__":
     app.run(debug=True)
