6cd32c425c1b48d58732b0653656a6d41b1806e2
Smasia <sander130506@gmail.com>
2024-05-22 10:55:05 +0200

    Lagde script for database

--- server/app.py
+++ server/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory, json, redirect, session
-import json
-import requests
+from flask import Flask, request
 import sqlite3
-import os
-from werkzeug.utils import secure_filename
 from datetime import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-# BYTT PATH PÅ DATABASEN    
+
 con = sqlite3.connect("database.db", check_same_thread=False)
 cur = con.cursor()
+
+@app.route('/get_tema')
+def get_tema():
+  return 200
+
---
+++ server/database.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+con = sqlite3.connect('database.db', check_same_thread=False)
+cur = con.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS tema(
+            id INTEGER PRIMARY KEY NOT NULL,
+            tema TEXT NOT NULL
+)""")
+con.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS guides(
+            id INTEGER PRIMARY KEY NOT NULL,
+            tema_id INTEGER NOT NULL,
+            bruker_id INTEGER NOT NULL,
+            innhold TEXT NOT NULL,
+            tittel TEXT NOT NULL,
+            dato TIMESTAMP
+)""")
+con.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS brukere(
+            id INTEGER PRIMARY KEY NOT NULL,
+            navn TEXT NOT NULL UNIQUE,
+            passord TEXT NOT NULL
+)""")
+con.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS kommentarer(
+            id INTEGER PRIMARY KEY NOT NULL,
+            bruker_id INTEGER NOT NULL,
+            guide_id INTEGER NOT NULL,
+            innhold TEXT NOT NULL,
+            rating INTEGER NOT NULL,
+            dato TIMESTAMP
+)""")
+con.commit()
+
