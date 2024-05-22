048468d532665518f90e88c40a2cec81bdb548d4
Smasia <sander130506@gmail.com>
2024-05-22 11:06:02 +0200

    La til alle endepunktene vi trenger for nå

--- server/app.py
+++ server/app.py
@@ -10,6 +10,38 @@ con = sqlite3.connect("database.db", check_same_thread=False)
 cur = con.cursor()
 
 
-@app.route('/get_tema')
+@app.route('/registrer', methods=["POST"])
+def registrer():
+  return 200
+
+@app.route('/logg_inn', methods=["GET"])
+def logg_inn():
+  return 200
+
+@app.route('/get_tema', methods=["GET"])
 def get_tema():
+  return 200
+
+@app.rotue('/post_tema', methods=["POST"])
+def post_tema():
+  return 200
+
+@app.route('/get_guides', methods=["POST"])
+def get_guides():
+  return 200
+
+@app.route('/get_guide', methods=["GET"])
+def get_guide():
+  return 200
+
+@app.route('/post_guide', methods=["POST"])
+def post_guides():
+  return 200
+
+@app.route('/get_kommentarer', methods=["POST"])
+def get_kommentarer():
+  return 200
+
+@app.route('/post_kommentarer', methods=["POST"])
+def post_kommentarer():
   return 200
